63b9a1fd8209ef0049d77b3a4816a082aaf60b4b
alessioiaretti <iaretti.alessio@iisgalvanimi.edu.it>
2020-04-23 13:36:31 +0000

    01.1

---
+++ angular-routing/src/environments/environment.ts
@@ -0,0 +1,17 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  oauthToken: "Bearer  BQDuHL_XB1V_1vDBlDo-8FEV3Edr21KAeYnwwjRZGKn_W1Ta-jOZdsxJhCmaJx5q8syk9Z5aCizXSMutw99oFcK56pqPIrva44ogTdcW07ORn2wDve1tN84i7H3SKS_mX2oqWUtSSXOW-t_kHdZF3L2eG3cfoNZ-mb_ouxGV_7-I7SpQ"
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
